a8981b4bfc135356e35f481a4d121a0bdf238f16
seiya.oshiro <seiya.oshiro@monicle.co.jp>
2024-08-09 18:14:27 +0900

    when using Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
 /// <reference types="vitest" />
 import { vitePlugin as remix } from "@remix-run/dev";
+import react from '@vitejs/plugin-react'
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    remix({
+    // https://remix.run/docs/en/main/guides/vite#plugin-usage-with-other-vite-based-tools-eg-vitest-storybook
+    !process.env.VITEST ? remix({
       ssr: false,
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
       },
-    }),
+    }) : react(), 
     tsconfigPaths(),
   ],
   test: {
